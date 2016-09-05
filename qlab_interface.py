e5fd7a9cf4590f2e0cb215ef7b067086d574a49a
Harry <hjwp2@cantab.net>
2016-09-05 18:22:48 +0200

    tweaks to renumberer

--- qlab_interface.py
+++ qlab_interface.py
@@ -18,14 +18,16 @@ class Listener:
 
 
     def _get_message(self):
-        data, address = self.sock.recvfrom(4096)
+        data, address = self.sock.recvfrom(8192)
         raw = data.decode('utf8')
-        parts = filter(bool, raw.split('\x00'))
-        json_message = list(parts)[2]
+        parts = list(filter(bool, raw.split('\x00')))
+        json_message = parts[2]
         try:
             self.last_message = json.loads(json_message)
         except json.decoder.JSONDecodeError as e:
             print('Error. server raw response:', repr(raw))
+            print('parts', parts)
+            print(e)
             self.last_message = None
 
     def get_message(self):
@@ -94,48 +96,59 @@ def _fix_simple_cue_numbers():
         interface.client.send_message('/select/next')
 
 
-def process_group(interface, group_cue_no, prefix):
+def process_group(interface, group_cue_no):
     group_cues = interface.get_cue_property(group_cue_no, 'children')
     subgroup_no = 0
     item_no = 0
     for cue_info in group_cues:
         cue_no = cue_info.get('number')
         item_no += 1
-        new_no = '{}.{}'.format(prefix, item_no)
+        new_no = '{}.{}'.format(group_cue_no, item_no)
         cue_name = interface.get_cue_property(cue_no, 'name')
         cue_type = interface.get_cue_property(cue_no, 'type')
 
-        if cue_type == 'Group':
-            process_group(interface, cue_no, new_no)
-
-        elif cue_type == 'Fade':
+        if cue_type == 'Fade':
             target = interface.get_cue_property(cue_no, 'cueTargetNumber')
             new_no = '{}.off'.format(target)
 
+        interface.set_cue_property(cue_no, 'number', new_no)
         if cue_name:
             print(cue_no, new_no, cue_name)
         else:
-            text = interface.get_cue_property(cue_no, 'text')
+            text = interface.get_cue_property(new_no, 'text')
             print(cue_no, new_no, repr(text))
-        interface.client.send_message('/select/{}'.format(cue_no))
+        interface.client.send_message('/select/{}'.format(new_no))
+
+        if cue_type == 'Group':
+            process_group(interface, new_no)
+
 
 
 
 
 def main():
     interface = Interface()
-    interface.client.send_message('/select/3.5.58')
-    current_group_cue_no = None
-    item_no = 0
 
-    for _ in range(20):
-        item_no += 1
+    start_act = 4
+    start_scene = 1
+    interface.client.send_message('/select/{act}.{scene}.1'.format(act=start_act, scene=start_scene))
+
+    current_group_cue_no = None
+    last_act, last_scene = None, None
+    while True:
         cue_no = interface.get_cue_property('selected', 'number')
         act, scene, _ = cue_no.split('.', 3)
+        if act != last_act or scene != last_scene:
+            item_no = 1
+        else:
+            item_no += 1
+        last_act, last_scene = act, scene
+
         cue_name = interface.get_cue_property('selected', 'name')
         cue_type = interface.get_cue_property('selected', 'type')
         new_no  = '{act}.{scene}.{item_no}'.format(**locals())
 
+        interface.set_cue_property(cue_no, 'number', new_no)
         if cue_name:
             print(cue_no, new_no, cue_name)
         else:
@@ -143,7 +156,10 @@ def main():
             print(cue_no, new_no, repr(text))
 
         if cue_type == 'Group':
-            process_group(interface, cue_no, new_no)
+            process_group(interface, new_no)
+
+        if cue_no == '41551':
+            break
 
         interface.client.send_message('/select/next')
 
