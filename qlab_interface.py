7c05d236e25e47645c5e2cd6a98696da295947c0
Harry <hjwp2@cantab.net>
2016-09-05 12:25:42 +0200

    better debugging, set values, basic renumbering

--- qlab_interface.py
+++ qlab_interface.py
@@ -2,6 +2,7 @@
 import json
 import socket
 import sys
+import time
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
@@ -19,12 +20,12 @@ class Listener:
     def _get_message(self):
         data, address = self.sock.recvfrom(4096)
         raw = data.decode('utf8')
-        parts = raw.split('\x00')
-        json_message = parts[5]
+        parts = filter(bool, raw.split('\x00'))
+        json_message = list(parts)[2]
         try:
             self.last_message = json.loads(json_message)
         except json.decoder.JSONDecodeError as e:
-            print('Error. server response:', repr(json_message))
+            print('Error. server raw response:', repr(raw))
             self.last_message = None
 
     def get_message(self):
@@ -42,17 +43,17 @@ class Client:
         self.client = udp_client.UDPClient('localhost', 53000)
 
 
-    def send_message(self, address):
-        print('sending message to', address)
+    def send_message(self, address, value=None):
         msg = osc_message_builder.OscMessageBuilder(address=address)
+        if value:
+            msg.add_arg(value)
         self.client.send(msg.build())
 
 
     def get_cue_text(self, cue_no):
-        print('getting cue text', cue_no)
         self.send_message(address='/cue/{}/text'.format(cue_no))
 
-
+    
 
 class Interface:
     def __init__(self):
@@ -67,16 +68,57 @@ class Interface:
     def get_cue_property(self, cue_no, name):
         print('getting cue {} property {}'.format(cue_no, name))
         self.client.send_message('/cue/{cue_no}/{name}'.format(**locals()))
-        return self.server.get_message()
+        response = self.server.get_message()
+        if response:
+            return response.get('data')
 
+
+    def set_cue_property(self, cue_no, name, value):
+        print('setting cue {} property {} to {}'.format(cue_no, name, value))
+        self.client.send_message('/cue/{cue_no}/{name}'.format(**locals()), value=value)
+
+
+def _fix_simple_cue_numbers():
+    interface.client.send_message('/select/31003')
+    while True:
+        cue_no = interface.get_cue_property('selected', 'number')
+        print(cue_no)
+        if cue_no:
+            act = int(cue_no[:1])
+            if act > 3:
+                break
+            scene = int(cue_no[1:3])
+            number = int(cue_no[3:])
+            new_no = '{}.{}.{}'.format(act, scene, number)
+            interface.set_cue_property('selected', 'number', new_no)
+            
+
+        interface.client.send_message('/select/next')
 
 
 def main():
     interface = Interface()
-    cue_text = interface.get_cue_text(10102)
-    print(cue_text)
-    cue_surface = interface.get_cue_property(10102, 'surfaceID')
-    print(cue_surface)
+    interface.client.send_message('/select/3.1.1')
+    group_no = 0
+    for _ in range(20):
+        cue_no = interface.get_cue_property('selected', 'number')
+        act, scene, number = cue_no.split('.', 3)
+        cue_name = interface.get_cue_property('selected', 'name')
+        cue_type = interface.get_cue_property('selected', 'type')
+        if cue_type == 'Group':
+            if 'Group' in cue_name:
+                group_no += 1
+                subgroup_no = 0
+                item_no = 0
+                new_no = '{act}.{scene}.{group_no}'.format(**locals())
+            else:
+                subgroup_no += 1
+                new_no = '{act}.{scene}.{group_no}.{subgroup_no}'.format(**locals())
+        else:
+            item_no += 1
+            new_no = '{group_name}.{item_no}'.format(group_name=new_no, item_no=item_no)
+        print(cue_no, new_no)
+        interface.client.send_message('/select/next')
 
 
 if __name__ == '__main__':
